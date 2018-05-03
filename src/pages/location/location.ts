eb6643efb43a82c74cf6be21f587fef5d7b70ebb
Uzzy97 <g00345816@gmit.ie>
2018-05-03 14:33:45 +0100

    Comments, Formatted and Finished

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -16,33 +16,33 @@ export class LocationPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
+  // Show Map
   ionViewDidLoad() {
     this.showMap();
   }
-  showMap(){
+  showMap() {
     //Location - lat long
-    const location = new google.maps.LatLng(53.767364,-8.4907087,17);
+    const location = new google.maps.LatLng(53.767364, -8.4907087, 17);
 
-      // Map Options
-      const options = {
-        center: location,
-        zoom:15
-      };
+    // Map Options
+    const options = {
+      center: location,
+      zoom: 15
+    };
 
-      const map = new google.maps.Map(this.mapRef.nativeElement,
-        options);
+    const map = new google.maps.Map(this.mapRef.nativeElement,
+      options);
+    // Timer
+    setTimeout(() => {
+      map.setMapTypeId('roadmap');
+    }, 5000);
 
-        setTimeout(() => {
-          map.setMapTypeId('roadmap');
-        }, 5000);
-
-
-        this.addMarker(location, map);
+    // Add Marker
+    this.addMarker(location, map);
 
   }
-
-  addMarker(position, map){
+  // Position Marker
+  addMarker(position, map) {
     return new google.maps.Marker({
       position,
       map
--- src/pages/service/service.ts
+++ src/pages/service/service.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { EmailComposer } from '@ionic-native/email-composer';
 
